f1e9a653b2a76eb657b70a39ef6e44311e5c2cf2
Stephanie Oliveira <stearruda@gmail.com>
2018-07-17 23:44:42 +0200

    style: change double quote to single quote

--- src/components/ListOfBooks.js
+++ src/components/ListOfBooks.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 
 class ListOfBooks extends React.Component {
+
 	render() {
 		return (
 			<div className="list-books">
