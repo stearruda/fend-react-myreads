7dfd4a231528b3d682d120f86a743a022a37a51d
Stephanie Oliveira <stearruda@gmail.com>
2018-07-21 14:31:51 +0200

    feat: load Books from the Server and show them to the page

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,33 +1,33 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 
-class SearchBooks extends React.Component{
+class SearchBooks extends React.Component {
 	render() {
 		return (
-			<div className="search-books">
-				<div className="search-books-bar">
-				  <Link
-				  	className="close-search"
-				  	to='/'
-				  >Close</Link>
-				  <div className="search-books-input-wrapper">
-				    {
-				      // NOTES: The search from BooksAPI is limited to a particular set of search terms.
-				      // You can find these search terms here:
-				      // https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+			<div className='search-books'>
+				<div className='search-books-bar'>
+					<Link
+						className='close-search'
+						to='/'
+					>Close</Link>
+					<div className='search-books-input-wrapper'>
+						{
+							// NOTES: The search from BooksAPI is limited to a particular set of search terms.
+							// You can find these search terms here:
+							// https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-				      // However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-				      // you don't find a specific author or title. Every search is limited by search terms.
-				    }
-				    <input type="text" placeholder="Search by title or author"/>
+							// However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+							// you don't find a specific author or title. Every search is limited by search terms.
+						}
+						<input type='text' placeholder='Search by title or author'/>
 
-				  </div>
+					</div>
 				</div>
-				<div className="search-books-results">
-				  <ol className="books-grid"></ol>
+				<div className='search-books-results'>
+					<ol className='books-grid'></ol>
 				</div>
-			</div>		
+			</div>
 		)
 	}
 }
